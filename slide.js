efafc54a5703997cd3332620fe5049d4478024c5
Clarice Ellen <141864736+clarice1337@users.noreply.github.com>
2023-11-29 21:50:52 -0300

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,19 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaIMG()
+},3500)
+
+function proximaIMG(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+
+}
